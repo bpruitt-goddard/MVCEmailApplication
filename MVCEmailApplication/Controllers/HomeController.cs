1d2ac7c05f0514a9a9ea21f666f0a5c6c83c3c4d
Brian Pruitt-Goddard <bpruitt@trackabout.com>
2015-11-30 23:12:16 -0500

    Display logged SystemEmails on Home page

--- MVCEmailApplication/Controllers/HomeController.cs
+++ MVCEmailApplication/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MVCEmailApplication.Models;
 
 namespace MVCEmailApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            //User must be authorized by this point. Safe to retrieve user id
+            var userId = User.Identity.GetUserId();
+            var user = db.Users.Single(u => u.Id == userId);
+
+            var viewModel = new SystemEmailUserViewModel
+            {
+                User = user,
+                SystemEmails = db.SystemEmails.Where(se => se.UserId == user.Id)
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ MVCEmailApplication/Models/SystemEmailUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEmailApplication.Models
+{
+    public class SystemEmailUserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public IEnumerable<SystemEmail> SystemEmails { get; set; }
+    }
+}
